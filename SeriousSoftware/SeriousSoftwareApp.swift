6c7022bd2e1b0e1c75f8e4acbe43fce5ead2704f
Alex Bibikov <subselect@gmail.com>
2022-04-28 16:23:18 +0300

    Initial Commit

---
+++ SeriousSoftware/SeriousSoftwareApp.swift
@@ -0,0 +1,17 @@
+//
+//  SeriousSoftwareApp.swift
+//  SeriousSoftware
+//
+//  Created by Alex Bibikov on 28.04.2022.
+//
+
+import SwiftUI
+
+@main
+struct SeriousSoftwareApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
